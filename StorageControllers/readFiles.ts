430b3279df96851754f54b0e85230f4c98496366
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-10-06 20:46:46 +0300

    read an image from storage and return as base64

--- StorageControllers/readFiles.ts
+++ StorageControllers/readFiles.ts
@@ -14,3 +14,24 @@ const blobServiceClient = new BlobServiceClient(
 );
 
 const containerClient = blobServiceClient.getContainerClient(config.imagesContainerName);
+
+async function getImageById(id : string) : Promise<string>{
+
+    let j = 1;
+    var blockBlobClient = containerClient.getBlockBlobClient(id);
+    var downloadBlockBlobResponse = await blockBlobClient.downloadToBuffer();
+    return downloadBlockBlobResponse.toString('base64');
+}
+
+async function streamToBuffer(readableStream) {
+    return new Promise((resolve, reject) => {
+      const chunks = [];
+      readableStream.on("data", (data) => {
+        chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+      });
+      readableStream.on("end", () => {
+        resolve(Buffer.concat(chunks));
+      });
+      readableStream.on("error", reject);
+    });
+  }

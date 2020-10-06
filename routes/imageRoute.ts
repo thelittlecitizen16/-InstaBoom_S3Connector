2a03a5893ef2ec29778de8a56d7847cf6ed607c2
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-10-06 22:15:25 +0300

    bug fixed

--- routes/imageRoute.ts
+++ routes/imageRoute.ts
@@ -4,11 +4,7 @@ var router = express.Router();
 
 router.post('/getphotos', async (req, res) => {
     let entities = req.body.entities;
-    //console.log(entities);
     let photos = await fileReader.getMultipleImages(entities);
-    //console.log(photos);
-    
-    //let photosJSON = JSON.parse(photos);
     res.status(200).send(photos);
 })
 

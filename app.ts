0ce5b2b1323f8d94455b4fcf93bd35f140fc4165
lil cit <thelittlecitizen3@gmail.com>
2020-10-13 15:15:06 +0300

    added https connection and selfsigned certificate

--- app.ts
+++ app.ts
@@ -1,15 +1,27 @@
 'use strict';
 
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('./ssl/key.pem', 'utf8');
+var certificate = fs.readFileSync('./ssl/cert.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
 import express = require('express');
 import config from './configuration.json'; 
 const app : express.Application = express();
 const port = config.port;
+const httpsPort = config.httpsPort;
+
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(require('./routes/imageRoute.ts'));
 
-app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`)
-  })
-  
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+
+httpServer.listen(port);
+httpsServer.listen(httpsPort);
